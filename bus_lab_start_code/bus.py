67fc2203c4e152cff54ae0d9affeb554a11a3c3d
John Basile <ashton.basile@gmail.com>
2023-07-11 09:52:01 +0100

    I finished Part B

--- bus_lab_start_code/bus.py
+++ bus_lab_start_code/bus.py
@@ -2,6 +2,19 @@ class Bus:
 	def __init__(self, route_num, destination):
 		self.route_num = route_num
 		self.destination = destination
+		self.passengers = []
 
-	def drive():
+	def drive(self):
 		return "Brum brum"
+
+	def get_num_passengers(self):
+		return len(self.passengers)
+
+	def pick_up(self, passenger):
+		self.passengers.append(passenger)
+
+	def drop_off(self, passenger):
+		self.passengers.remove(passenger)
+	
+	def empty(self):
+		self.passengers = []
